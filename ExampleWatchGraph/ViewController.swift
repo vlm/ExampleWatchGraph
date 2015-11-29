13047b9708f223c86e9b5bdb37bb22eb036925e6
Lev Walkin <vlm@lionet.info>
2015-11-29 14:53:40 -0800

    Initial Commit

---
+++ ExampleWatchGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleWatchGraph
+//
+//  Created by Lev Walkin on 11/29/15.
+//  Copyright © 2015 Lev Walkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# About
+
+This Apple Watch App (for WatchOS 2.0) demonstrates use of CoreGraphics.
+
+# To Build
+
+ 1. Open the `ExampleWatchGraph.xcodeproj` using the XCode.
+ 2. Configure the `Product → Scheme` to `ExampleWatchGraph WatchKit App`
+ 3. Run `⌘R`.
+
